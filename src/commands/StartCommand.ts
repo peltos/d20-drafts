a65cf93a04a4f109fbfece105ac5b3d024511f87
Ron Pelt <pelt8@hotmail.com>
2021-02-20 11:09:12 +0100

    more console messages

--- src/commands/StartCommand.ts
+++ src/commands/StartCommand.ts
@@ -2,19 +2,28 @@ import { Message } from "discord.js";
 import StoryModel from "../models/StoryModel";
 import ConsoleTimeComponent from "../components/ConsoleTimeComponent";
 import CONFIG from "../../config";
-import { ANSI_RESET, ANSI_FG_YELLOW } from "../resources/ANSIEscapeCode";
+import { ANSI_RESET, ANSI_FG_YELLOW, ANSI_FG_GREEN } from "../resources/ANSIEscapeCode";
 
 const StartCommand = async (message: Message, Stories: StoryModel[]) => {
+  // send to console
+  ConsoleTimeComponent(
+    ANSI_FG_YELLOW,
+    "START ",
+    ANSI_RESET,
+    "command activated"
+  );
+
   // current message
   const timeTaken = Date.now() - message.createdTimestamp;
   await message
     .reply(`The story starts here! chose the route you want! ${timeTaken}`)
     .then(async (msg) => {
+      // send to console
       ConsoleTimeComponent(
-        ANSI_FG_YELLOW,
-        "START ",
-        ANSI_RESET,
-        "command activated"
+        "Message send ",
+        ANSI_FG_GREEN,
+        "succesful",
+        ANSI_RESET
       );
 
       Stories.push({
