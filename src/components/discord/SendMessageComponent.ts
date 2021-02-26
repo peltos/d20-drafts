378dee1983b99baaa5638c938ecc0d6167878cd1
Ron Pelt <pelt8@hotmail.com>
2021-02-27 00:25:01 +0100

    Added visual dice

--- src/components/discord/SendMessageComponent.ts
+++ src/components/discord/SendMessageComponent.ts
@@ -9,7 +9,7 @@ const SendMessageComponent = (
   story: StoryModel
 ) => {
   // current message
-  channel.send(story.content[0].content).then((msg) => {
+  channel.send(`${AsciiDice(20)} ${story.content[0].content}`).then((msg) => {
     // send to console
     ConsoleTimeComponent("Message send ", ANSI_FG_GREEN, "succesful", ANSI_RESET);
 
@@ -17,6 +17,34 @@ const SendMessageComponent = (
       await (msg as Message).react(rection.emoji);
     });
   });
+};
+
+const AsciiDice = (num: number) => {
+  if (num > 20 || num < 1) return "";
+
+  let c;
+  if (num === 20) c = "+";
+  else if (num === 1) c = "-";
+  else c = "|";
+
+  let numStr;
+  if (num.toString().length === 1) numStr = ` ${num} `;
+  else if (num.toString().length >= 2) {
+    let double = num.toString().split("");
+    numStr = `${double[0]} ${double[1]}`;
+  }
+
+  const dice = `\`\`\`diff
+${c}     _ --- _     ${c}
+${c}   -   / \\   -   ${c}
+${c}  |\\ /_____\\ /|  ${c}
+${c}  | /\\     /\\ |  ${c}
+${c}  |/  \\${numStr}/  \\|  ${c}
+${c}  |____\\ /____|  ${c}
+${c}   - _  |  _ -   ${c}
+${c}       ---       ${c}\`\`\``;
+
+  return dice;
 };
 
 export default SendMessageComponent;
