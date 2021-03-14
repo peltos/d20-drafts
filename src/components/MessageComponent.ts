10d671c302b3b0eecb71f25aca8da76758dc3075
Ron Pelt <pelt8@hotmail.com>
2021-03-15 00:49:45 +0100

    reconfig MessageComponent

--- src/components/MessageComponent.ts
+++ src/components/MessageComponent.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, Message } from "discord.js";
 import Store from "../store/Store";
 import CONFIG from "../../config";
 import StartCommand from "../commands/StartCommand";
@@ -11,58 +11,63 @@ export default class MessageComponent {
     // When A message is sent
     client.on("message", async (message) => {
       if (message.author.bot) {
-        // Listen for the bot
-        Store.PlotPointCount[Store.PlotPointCount.length - 1].messageId = message.id;
-        setTimeout(() => {
-          message.channel.fetchMessage(message.id).then((fetchMessage) => {
-            const next = new MessageNextComponent(
-              fetchMessage,
-              Store.PlotPointCount[Store.PlotPointCount.length - 1].plotPointId
-            );
-            Store.PlotPointCount[Store.PlotPointCount.length - 1].plotPointId =
-              next.plotPointId;
-          });
-          
-        }, 5000); // MiliSeconds
+        this.botMessage(message);
       } else {
-        // Listen for commands
-
-        // Check if a command is used
-        if (!message.content.startsWith(CONFIG.prefix)) return;
-
-        // Prepare the command
-        const commandBody = message.content.slice(CONFIG.prefix.length);
-        const args = commandBody.split(" ");
-
-        if (args.shift() !== "d20d") return;
-
-        // send to console
-        new ConsoleTimeComponent(
-          ANSI_FG_CYAN,
-          "Discord Client Message ",
-          ANSI_RESET,
-          "event"
-        );
-
-        const command = args.shift()?.toLowerCase();
-
-        // Commands
-        switch (command) {
-          case "start":
-            new StartCommand(message, args[0], args[1]);
-            break;
-          default:
-            new ConsoleTimeComponent(
-              ANSI_FG_RED,
-              "Command ",
-              ANSI_RESET,
-              `${command?.toUpperCase() as string} `,
-              ANSI_FG_RED,
-              "not found",
-              ANSI_RESET
-            );
-        }
+        this.userMessage(message);
       }
     });
   }
+
+  private botMessage = (message: Message) => {
+    // Listen for the bot
+    Store.PlotPointCount[Store.PlotPointCount.length - 1].messageId = message.id;
+    setTimeout(() => {
+      message.channel.fetchMessage(message.id).then((fetchMessage) => {
+        const next = new MessageNextComponent(
+          fetchMessage,
+          Store.PlotPointCount[Store.PlotPointCount.length - 1].plotPointId
+        );
+        Store.PlotPointCount[Store.PlotPointCount.length - 1].plotPointId =
+          next.plotPointId;
+      });
+    }, 5000); // MiliSeconds
+  };
+
+  private userMessage = (message: Message) => {
+    // Check if a command is used
+    if (!message.content.startsWith(CONFIG.prefix)) return;
+
+    // Prepare the command
+    const commandBody = message.content.slice(CONFIG.prefix.length);
+    const args = commandBody.split(" ");
+
+    if (args.shift() !== "d20d") return;
+
+    // send to console
+    new ConsoleTimeComponent(
+      ANSI_FG_CYAN,
+      "Discord Client Message ",
+      ANSI_RESET,
+      "event"
+    );
+
+    const command = args.shift()?.toLowerCase();
+
+    // Commands
+    switch (command) {
+      case "start":
+        new StartCommand(message, args[0], args[1]);
+        break;
+      default:
+        new ConsoleTimeComponent(
+          ANSI_FG_RED,
+          "Command ",
+          ANSI_RESET,
+          `${command?.toUpperCase() as string} `,
+          ANSI_FG_RED,
+          "not found",
+          ANSI_RESET
+        );
+    }
+  };
 }
