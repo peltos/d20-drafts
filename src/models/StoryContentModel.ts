8ce807ac60e4a293d79e4db12a6d291b89d963a8
LAPTOP-QE3P27NA\Noa Roos <noaroos01@gmail.com>
2021-03-25 21:52:10 +0100

    Added support for images in bot messages. Image locations from internet in stories json file.

--- src/models/StoryContentModel.ts
+++ src/models/StoryContentModel.ts
@@ -1,6 +1,7 @@
 import StoryReactionsModel from "./StoryReactionsModel";
 
 export default interface StoryContentModel {
+  file_destination: string;
   plotPointId: number;
   content: string;
   reactions: StoryReactionsModel[];
