cc1099fba3f6672de3682f80b75903283763a05f
Ron Pelt <pelt8@hotmail.com>
2023-03-23 14:06:47 +0100

    website

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  build: {
+    outDir: './docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
